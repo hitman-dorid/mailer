0172742992bbca50af651a308bbbd225cafeccf7
Anik <mosheur_rahman15@gmail.com>
2019-07-08 00:59:55 +0600

    modified command line argument bugs

--- mailer.py
+++ mailer.py
@@ -8,17 +8,19 @@
 
 # EMAIL : mosheur.rahman.15@gmail.com
 # FACEBOOK : https://www.facebook.com/mosheur.rahman.anik
-
-import sys, time, smtplib, socket, os
+# GITHUB : https://github.com/hitman-dorid/mailer
+import sys, time, smtplib, socket, os, argparse
 
 banner = """
-
+\033[32m
     _ ___    __ _
 |V||_| | |  |_ |_)
 | || |_|_|__|__| \
 v.1.0.1
-
+\033[0m
 AUTHOR:mosheur_rahman!
+
+GITHUB:https://github.com/hitman-dorid/mailer
 
 """
 
@@ -26,30 +28,6 @@ gmail = ['smtp.gmail.com',587]
 yahoo = ['smtp.mail.yahoo.com',587]
 outlook = ['smtp-mail.outlook.com',587]
 
-help = """
-mailer is a script for sending email from comand line!
-
-specaly desined for termux !
-
-type 'mailer' for run it !
-
-or use command line arguments !
-
-arguments list:
-
-[-i] = used to put user email id!
-
-[-p] = used to  put user email password!
-
-[-t] = used to put target mail address!
-
-[-m] = used to put  the message to send!
-
-[-sn]= used to put the smtp server address of user!
-
-[-sp]= usee to put the smtp server port of user !
-
-"""
 def main (id, pas, target, msg, ser_name, ser_port):
 	"""
 	EMAIL CLIENT SIDE FUNCTION
@@ -58,31 +36,33 @@ def main (id, pas, target, msg, ser_name, ser_port):
 	try:
 		os.system('clear')
 		writer (banner)
-		writer (f'\nconnecting with {ser_name}\n')
+		writer (f'\033[32m\nconnecting with {ser_name}\n\033[0m')
 		client = smtplib.SMTP(ser_name,ser_port)
-		writer (f'\nconneted with {ser_name}\n')
-		writer ('\nchacking connection power...\n\n')
+		writer (f'\033[32m\nconneted with {ser_name}\n\033[0m')
+		writer ('\033[32m\nchacking connection power...\n\n\033[0m')
 		client.ehlo()
-		writer ('\n\nchacked\n')
-		writer (f'\nstarting TLS incription at {ser_name}\n')
+		writer ('\033[32m\n\nchacked\n\033[0m')
+		writer (f'\033[32m\nstarting TLS incription at {ser_name}\n\033[0m')
 		client.starttls()
-		writer (f'\nincription started at {ser_name}\n')
-		writer (f'\nlogging in {id}\n')
+		writer (f'\033[32m\nincription started at {ser_name}\n\033[0m')
+		writer (f'\033[32m\nlogging in {id}\n\033[0m')
 		client.login(id, pas)
-		writer (f'\nsucsessfuly loged in {id}\n')
-		writer ('\nsending mail\n')
+		writer (f'\033[32m\nsucsessfuly loged in {id}\n\033[0m')
+		writer ('\033[32m\nsending mail\n\033[0m')
 		client.sendmail(id, target, msg)
-		writer ('\nmail sent\n')
-		writer ('\nclosing connection \n')
+		writer ('\033[32m\nmail sent\n\033[0m')
+		writer ('\033[32m\nclosing connection \n\033[0m')
 		client.quit()
-		writer ('\ncolsed\n')
+		writer ('\033[32m\ncolsed\n\033[0m')
 
 	except socket.gaierror:
-		writer ('no internet')
+		writer ('\n\033[31m\nno internet\n\033[0m')
 	except smtplib.SMTPAuthenticationError:
-		writer ('email/pass error')
+		writer ('\033[31m\nemail/pass error\n\033[0m')
 	except smtplib.SMTPServerDisconnected:
-		writer ('server disconnected')
+		writer ('\033[31m\nserver disconnected\n\033[0m')
+	except TypeError:
+		writer ('\n\033[31mtype error! please chack your args\n\033[0m')
 
 
 def writer (data):
@@ -111,7 +91,7 @@ def service_handler (service_name):
 		elif name == 'outlook.com':
 			return outlook
 		else:
-			return 'not found'
+			return None,None
 	else:
 		writer ('\ninvalid address..\n')
 		sys.exit(0)
@@ -119,110 +99,99 @@ def service_handler (service_name):
 def input_handler ():
 
 	"""
-	INPUT TAKER FOR RUNTIME
+	INPUT TAKEING
 	"""
 	try:
 		os.system('clear')
 		writer (banner)
-		writer ('\nenter your email address:\n')
-		id = input ('\n>> ')
-		writer (f'\nenter password for {id}\n')
-		pas = input ('\n>> ')
-		writer ('\nenter target email address:\n')
-		target = input ('\n>> ')
-		writer ('\nenter your message :\n')
-		msg = input ('\n>> ')
+		writer ('\033[32m\nenter your email address:\n\033[0m')
+		id = input ('\033[32m\n>> \033[0m')
+		writer (f'\033[32m\nenter password for {id}\n\033[0m')
+		pas = input ('\033[0m\n>> \033[0m')
+		writer ('\033[32m\nenter target email address:\n\033[0m')
+		target = input ('\033[32m\n>> \033[0m')
+		writer ('\033[32m\nenter your message :\n\033[0m')
+		msg = input ('\033[32m\n>> \033[0m')
 		id_chack = service_handler (id)
 		if id_chack == 'not found':
-			writer (f'\n please use castom\n')
-			writer ('\nenter your service smtp address:\n')
-			ser_name = input ('\n>> ')
-			writer (f'\nenter port for {ser_name}\n')
-			ser_port = input ('\n>> ')
+			writer ('\033[32m\n please use castom\n\033[0m')
+			writer ('\033[32m\nenter your service smtp address:\n\033[0m')
+			ser_name = input ('\033[32m\n>> \033[0m')
+			writer (f'\033[32m\nenter port for {ser_name}\n\033[0m')
+			ser_port = input ('\033[32m\n>> \033[0m')
 			main (id, pas, target, msg, ser_name, ser_port)
 		else:
 			(ser_name, ser_port) = id_chack
 			main (id, pas, target, msg, ser_name, ser_port)
 	except KeyboardInterrupt:
-		writer ('\ncanceled by user\n')
+		writer ('\033[31m\ncanceled by user\n\033[0m')
 		sys.exit(0)
 	except EOFError:
-		writer ('\ncanceled by user\n')
+		writer ('\033[31m\ncanceled by user\n\033[0m')
 		sys.exit(0)
 
 	else:
 		pass
 
-def cmd_argv_handler ():
+
+def args ():
 
 	"""
-	SYSTEM ARGUMENTS HANDLER
-
+	COMMAND LINE ARGUMENT HANDLER!
+	"""
+	help_msg = """
+	\n\n\033[32m
+	mailer is a simple tool for sending email from
+	command line...
+	you can simply run it by "$ python mailer.py"!
+	or use the command line arguments\n\n\033[0m
 	"""
 
-	try:
+	par = argparse.ArgumentParser(description=help_msg)
+	par.add_argument('-i','--id',type=str,metavar='',help='used to put user mail id\n')
+	par.add_argument('-p','--pas',type=str,metavar='',help='used to put user passrord\n')
+	par.add_argument('-t','--target',type=str,metavar='',help='used to put an target\n')
+	par.add_argument('-m','--msg',type=str,metavar='',help='used to put the message\n')
+	par.add_argument('-sa','--s_address',type=str,metavar='',help='used given server address manualy name\n')
+	par.add_argument('-sp','--s_port',type=str,metavar='',help='used to put port for manualy specified  address\n')
 
-		argv = sys.argv
-		o = 0
-		id = ''
-		pas = ''
-		target = ''
-		msg = 'mailer has you'
-		ser_name = ''
-		ser_port = ''
-		for i in argv:
-			if i == '-i':
-				id = argv[o+1]
-				o = o + 1
-			elif i == '-p':
-				pas = argv[o+1]
-				o = o + 1
-			elif i == '--help':
-				writer (help)
-				sys.exit(0)
-			elif i == '-t':
-				target = argv[o+1]
-				o = o + 1
-			elif i == '-m':
-				msg = argv[o+1]
-				o = o + 1
-			elif i == '-sn':
-				ser_name = argv[o+1]
-				o = o + 1
-			elif i == '-sp':
-				ser_port = argv[o+1]
-				o = o + 1
-			else:
-				o = o + 1
-		if id == '' or pas == '' or target == '':
-			writer ('\ninvalid option \n\ntype --help for help\n')
-			sys.exit(0)
-		elif ser_name == '' or ser_port == '':
-			s = service_handler(id)
-			if s == 'not found':
-				writer ('\nenter server address manualy:\n')
-				ser_name = input ('\n>> ')
-				writer ('\nenter port:\n')
-				ser_port = input ('\n>> ')
-			else:
-				ser_name,ser_port = s
-		else:
-			writer ('\error to send mail\n')
-		main(id,pas,target,msg,ser_name,ser_port)
-	except IndexError:
-		writer ('\nif your arguments have special char please write them under ""\n')
-	else:
-		pass
+	args = par.parse_args()
+	return args.id,args.pas,args.target,args.msg,args.s_address,args.s_port
 
 
+def arg_controler():
 
-if __name__=='__main__':
-	a = 0
-	for i in sys.argv:
-		a = a + 1
-	if a>1:
-		cmd_argv_handler()
-	else:
+	"""
+	PUTING ARGUMENTS AT THE RIGHT PLACE!
+	"""
+	id,pas,target,msg,sa,sp = args()
+	if id ==None and pas==None and target == None and msg == None and sa == None and sp == None:
 		input_handler()
-
+		sys.exit(0)
+	else:
+		if id == None:
+			print ('\n\033[31merror! required  user id\n\033[0m')
+			sys.exit(0)
+		elif pas == None:
+			print ('\n\033[31merror! required  user pas\n\033[0m')
+			sys.exit(0)
+		elif target == None:
+			print ('\n\033[31merror! required  a target id\n\033[0m')
+			sys.exit(0)
+		elif msg == None:
+                        print ('\n\033[31merror! required  a messgae\n\033[0m')
+                        sys.exit(0)
+		elif sa == None or sp == None:
+			sa,sp = service_handler(id)
+			if sa == None and sp == None:
+				writer ('\n\033[31mplz input the smtp details \n\033[0m')
+				writer ('\033[32menter the smtp server address:\n\033[0m')
+				sa = input ('\033[32m\n>> \033[0m')
+				writer ('\n\033[32menter port:\n\033[0m')
+				sp = input ('\033[32m\n>> \033[0m')
+			else:
+				writer ('\n\033[31munknowen error\n\033[0m')
+		main (id,pas,target,msg,sa,sp)
+	sys.exit(0)
+arg_controler()
 
